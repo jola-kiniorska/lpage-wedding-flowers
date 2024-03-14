fb485de4441d4360ba563f011a8fc78ae14b51d1
Jola Klodowska <jola.klodowska@wp.pl>
2024-03-14 22:57:21 +0100

    add styles to second section

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import styles from './Header.module.scss';
 
 const Header = () => {
     return (
-        <div className={styles.header}>
+        <div className={styles.section_header}>
             <h2>"A woman is a <span>flower...</span>"</h2>
         </div>
     )
--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,13 +1,15 @@
 import Paragraph from "../Paragraph/Paragraph";
 import Title from "../Title/Title";
+import styles from './Intro.module.scss';
+import woman from '../../img/offers_2.jpg'
 
 const Intro = props => {
     return (
-        <div>
-            <div>
-                <img></img>
+        <div className={styles.section_intro}>
+            <div className={styles.section_element}>
+                <img src={woman} alt={'woman'}/>
             </div>
-            <div>
+            <div className={styles.section_element}>
                 <p>Ziemia śmieje się kwiatami. – Ralph Waldo Emerson</p>
                 <Title/>
                 <Paragraph/>
