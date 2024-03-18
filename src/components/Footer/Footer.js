6a632be83591a3159933ddbb97d92cebcaf70db7
Jola Klodowska <jola.klodowska@wp.pl>
2024-03-18 17:31:47 +0100

    create SocialMediaIcon component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,24 +4,17 @@ import instagram_icon from "../../img/icons/instagram_icon.svg"
 import linkedin_icon from "../../img/icons/linkedin_icon.svg"
 import twitter_icon from "../../img/icons/twitter_icon.svg"
 import Paragraph from "../Paragraph/Paragraph";
+import SocialMediaIcon from "../SocialMediaIcon/SocialMediaIcon";
 
 const Footer = () => {
     return (
         <div className={styles.section_footer}>
             <Paragraph text='Follow us on' />
             <div className={styles.footer_icons}>
-                <a href='https://www.instagram.com/'>
-                    <img src={instagram_icon} alt={instagram_icon}></img>
-                </a>
-                <a href='https://www.facebook.com/'>
-                    <img src={facebook_icon} alt={facebook_icon}></img>
-                </a>
-                <a href='https://twitter.com/'>
-                    <img src={twitter_icon} alt={twitter_icon}></img>
-                </a>
-                <a href='https://www.linkedin.com/'>
-                    <img src={linkedin_icon} alt={linkedin_icon}></img>
-                </a>
+                <SocialMediaIcon icon={facebook_icon} />
+                <SocialMediaIcon icon={instagram_icon} />
+                <SocialMediaIcon icon={linkedin_icon} />
+                <SocialMediaIcon icon={twitter_icon} />
             </div>
             <p className={styles.copyright_text}>Copyright 2024 rights reserved. Powered by React</p>
         </div>
